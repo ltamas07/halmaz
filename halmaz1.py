a2d35e381f63408f6ae3893efcb5f1654f2bb64d
Lévai Tamás <levai.tamas.2021c@ms.gdszeged.hu>
2022-11-18 10:36:55 +0100

    Halmaz Projekt

---
+++ halmaz1.py
@@ -0,0 +1,22 @@
+# reggeli = {'Víz', 'Tea', 'Vaj', 'Pirítós'}
+# ebed = {'Víz', 'Halászlé', 'Kenyér'}
+
+# print(reggeli & ebed)
+# print(reggeli | ebed)
+# print(reggeli - ebed)
+# print(reggeli ^ ebed)
+
+# gyumolcskosar = ['Eper', 'Alma', 'Szilva', 'Barack', 'Körte']
+# fajta = set()
+# for gyumolcs in gyumolcskosar:
+#     fajta.add(gyumolcs)
+# print(fajta)
+
+
+## FELADATOK @@
+
+
+vacsora = {'Krumplistarhonya', 'Húsleves', ''}
+print(vacsora)
+vacsora.clear()
+print(vacsora)
